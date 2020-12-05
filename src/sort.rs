93b5c1f611a47dcf99efde2cb64f152715b44493
adder46 <dedmauz69@gmail.com>
2020-12-05 14:50:53 +0100

    Tweak sort.rs a bit

--- src/sort.rs
+++ src/sort.rs
@@ -2,36 +2,36 @@ use std::cmp::{Eq, Reverse};
 use std::collections::HashMap;
 use std::hash::Hash;
 
-pub fn sort<T>(mut entries: Vec<T>) -> Vec<T>
+pub fn sort<T>(mut commands: Vec<T>) -> Vec<T>
 where
     T: Clone + Eq + Hash,
 {
-    let freq_map = frequency_map(&entries);
-    let pos_map = position_map(&entries);
-    entries.sort_by_key(|b| Reverse(pos_map.get(b).unwrap()));
-    entries.dedup();
-    entries.sort_by_key(|b| Reverse(freq_map.get(b).unwrap()));
-    entries
+    let freq_map = frequency_map(&commands);
+    let pos_map = position_map(&commands);
+    commands.sort_by_key(|c| Reverse(pos_map.get(c).unwrap()));
+    commands.dedup();
+    commands.sort_by_key(|c| Reverse(freq_map.get(c).unwrap()));
+    commands
 }
 
-fn frequency_map<T>(entries: &[T]) -> HashMap<T, usize>
+fn frequency_map<T>(commands: &[T]) -> HashMap<T, usize>
 where
     T: Clone + Eq + Hash,
 {
     let mut map = HashMap::new();
-    for e in entries.iter() {
-        *map.entry(e.clone()).or_insert(0) += 1;
+    for cmd in commands.iter() {
+        *map.entry(cmd.clone()).or_insert(0) += 1;
     }
     map
 }
 
-fn position_map<T>(entries: &[T]) -> HashMap<T, usize>
+fn position_map<T>(commands: &[T]) -> HashMap<T, usize>
 where
     T: Clone + Eq + Hash,
 {
     let mut map = HashMap::new();
-    for (pos, e) in entries.iter().enumerate() {
-        map.insert(e.clone(), pos);
+    for (pos, cmd) in commands.iter().enumerate() {
+        map.insert(cmd.clone(), pos);
     }
     map
 }
@@ -39,9 +39,9 @@ where
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_sort() {
+    fn sort() {
         let vec = vec![3, 2, 4, 6, 2, 4, 3, 3, 4, 5, 6, 3, 2, 4, 5, 5, 3];
-        let vec = super::sort(vec);
-        assert_eq!(vec, [3, 4, 5, 2, 6]);
+        let sorted_vec = super::sort(vec);
+        assert_eq!(sorted_vec, [3, 4, 5, 2, 6]);
     }
 }
